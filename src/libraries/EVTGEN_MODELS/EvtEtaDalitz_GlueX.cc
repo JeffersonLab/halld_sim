aa1df86f0f318d860facebbc983e4941619c15fb
Sean Dobbs <sdobbs@charm.physics.fsu.edu>
2019-07-31 11:29:49 -0400

    Add eta Dalitz model with higher order parameters

--- src/libraries/EVTGEN_MODELS/EvtEtaDalitz_GlueX.cc
+++ src/libraries/EVTGEN_MODELS/EvtEtaDalitz_GlueX.cc
@@ -24,50 +24,60 @@ EvtDecayBase* EvtEtaDalitz_GlueX::clone(){
 
 }
 
-void EvtEtaDalitz_GlueX::init(){
+void EvtEtaDalitz_GlueX::init() 
+{
+	// check that there are 7 arguments
+	checkNArg(7);
+	checkNDaug(3);
 
-  // check that there are 0 arguments
-  checkNArg(0);
-  checkNDaug(3);
+	checkSpinParent(EvtSpinType::SCALAR);
 
-  checkSpinParent(EvtSpinType::SCALAR);
-
-  checkSpinDaughter(0,EvtSpinType::SCALAR);
-  checkSpinDaughter(1,EvtSpinType::SCALAR);
-  checkSpinDaughter(2,EvtSpinType::SCALAR);
+	checkSpinDaughter(0,EvtSpinType::SCALAR);
+	checkSpinDaughter(1,EvtSpinType::SCALAR);
+	checkSpinDaughter(2,EvtSpinType::SCALAR);
 }
 
 
-void EvtEtaDalitz_GlueX::initProbMax(){
+void EvtEtaDalitz_GlueX::initProbMax()
+{
 
-  setProbMax(2.1);
+	setProbMax(2.15);
 
 }
 
-void EvtEtaDalitz_GlueX::decay( EvtParticle *p){
+void EvtEtaDalitz_GlueX::decay(EvtParticle *p)  
+{
 
-  p->initializePhaseSpace(getNDaug(),getDaugs());
+	p->initializePhaseSpace(getNDaug(), getDaugs());
 
-  // Will implement something new!
+	EvtVector4R mompip = p->getDaug(0)->getP4();
+	EvtVector4R mompim = p->getDaug(1)->getP4();
+	EvtVector4R mompi0 = p->getDaug(2)->getP4();
+	double masspip = p->getDaug(0)->mass();
+	double masspim = p->getDaug(1)->mass();
+	double masspi0 = p->getDaug(2)->mass();
+	double m_eta = p->mass();
 
-  EvtVector4R mompi0 = p->getDaug(2)->getP4();
-  double masspip = p->getDaug(0)->mass();
-  double masspim = p->getDaug(1)->mass();
-  double masspi0 = p->getDaug(2)->mass();
-  double m_eta = p->mass();
+	// Dalitz plot parameters
+	double Q = m_eta-masspip-masspim-masspi0;
+	double x = sqrt(3.)*( (mompip.get(0)-masspip) - (mompim.get(0)-masspim) )/Q; 
+	double y = (mompi0.get(0)-masspi0)*(3.0/Q)-1.0;
 
-  double y;
+	// pull out parameters from arguments
+	double a = getArg(0);
+	double b = getArg(1);
+	double c = getArg(2);
+	double d = getArg(3);
+	double e = getArg(4);
+	double f = getArg(5);
+	double g = getArg(6);
 
-  //The decay amplitude coems from Layter et al PRD 7 2565 (1973).
+	EvtComplex amp(sqrt(1.0 + a*y + b*y*y + c*x + d*x*x + e*x*y + f*y*y*y + g*x*x*y),0.0);
 
-  y=(mompi0.get(0)-masspi0)*(3.0/(m_eta-masspip-masspim-masspi0))-1.0;
+	vertex(amp);
 
-  EvtComplex amp(sqrt(1.0-1.07*y),0.0);
+	return;
 
-  vertex(amp);
-
-  return ;
-   
 }
 
 
