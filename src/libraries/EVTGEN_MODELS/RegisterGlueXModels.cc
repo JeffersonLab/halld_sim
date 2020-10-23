5f032caab9b4b749cd0989ccf8f47fa28dfd4e7d
jonzarling <jonzarling@gmail.com>
2020-10-23 09:17:53 -0400

    Add eta->3pi0 model and fix phase space factor in eta->pi+pi-gamma

--- src/libraries/EVTGEN_MODELS/RegisterGlueXModels.cc
+++ src/libraries/EVTGEN_MODELS/RegisterGlueXModels.cc
@@ -5,7 +5,9 @@
 #include "EvtGenBase/EvtModel.hh"
 
 #include "EvtEtaDalitz_GlueX.h"
+#include "EvtEtaDalitz_3Pi0_GlueX.h"
 #include "EvtEtaPiPiGamma.h"
+#include "EvtEtaPiPiGamma_GlueX.h"
 
 namespace GlueX_EvtGen 
 {
@@ -16,6 +18,7 @@ namespace GlueX_EvtGen
 
     // All new models must be added here, or EvtGen won't know them
     modelist.registerModel(new EvtEtaDalitz_GlueX);
+    modelist.registerModel(new EvtEtaDalitz_3Pi0_GlueX);
     modelist.registerModel(new EvtEtaPiPiGamma);
   }
 
