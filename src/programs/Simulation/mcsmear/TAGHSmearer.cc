4de3e33e5625d46030975f8ee9b625e08313f17f
Richard Jones <rjones30@gmail.com>
2023-02-09 18:48:00 -0500

    * update the tagger smearing to add error to the tagged photon   energy in both the microscope and hodoscope. [rtj]

--- src/programs/Simulation/mcsmear/TAGHSmearer.cc
+++ src/programs/Simulation/mcsmear/TAGHSmearer.cc
@@ -1,5 +1,6 @@
 #include "TAGHSmearer.h"
 
+tagh_config_t *tagh_config_instance(0);
 
 //-----------
 // tagh_config_t  (constructor)
@@ -19,8 +20,47 @@ tagh_config_t::tagh_config_t(JEventLoop *loop)
        int id = quality[i]["id"];
        counter_quality[id] = quality[i]["code"];
     }
+
+   std::map<string, float> beam_parms;
+   loop->GetCalib("PHOTON_BEAM/endpoint_energy", beam_parms);
+   endpoint_energy_GeV = beam_parms.at("PHOTON_BEAM_ENDPOINT_ENERGY");
+   std::map<string, float> beam_calib;
+   loop->GetCalib("PHOTON_BEAM/hodoscope/endpoint_calib", beam_calib);
+   endpoint_calib_GeV = endpoint_energy_GeV;
+   if (beam_calib.find("TAGGER_CALIB_ENERGY") != beam_calib.end()) {
+      endpoint_calib_GeV = beam_calib.at("TAGGER_CALIB_ENERGY");
+   }
+   std::vector<std::map<string, float> > hodo_parms;
+   loop->GetCalib("PHOTON_BEAM/hodoscope/scaled_energy_range", hodo_parms);
+   for (unsigned int i=0; i < hodo_parms.size(); ++i) {
+      int col = hodo_parms[i]["counter"];
+      double Emin = hodo_parms[i]["xlow"] * endpoint_calib_GeV
+                    + endpoint_energy_GeV - endpoint_calib_GeV;
+      double Emax = hodo_parms[i]["xhigh"] * endpoint_calib_GeV
+                    + endpoint_energy_GeV - endpoint_calib_GeV;
+      energy_range_GeV[col] = {Emin, Emax};
+   }
+   tagh_config_instance = this;
 }
 
+double TAGHSmearer::get_tagh_energy(int counter, int mid_low_high_rand)
+{
+   // mid_low_high_rand: =0 returns the center energy of the bin,
+   //                    =1 returns the low energy of the bin,
+   //                    =2 returns the high energy of the bin,
+   //                    =other returns a random value within the bin.
+   if (tagh_config_instance == 0)
+      return -1;
+   else if (mid_low_high_rand == 0)
+      return (tagh_config_instance->energy_range_GeV[counter][0] +
+              tagh_config_instance->energy_range_GeV[counter][1]) / 2;
+   else if (mid_low_high_rand == 1)
+      return tagh_config_instance->energy_range_GeV[counter][0];
+   else if (mid_low_high_rand == 2)
+      return tagh_config_instance->energy_range_GeV[counter][1];
+   return gDRandom.Uniform(tagh_config_instance->energy_range_GeV[counter][0],
+                           tagh_config_instance->energy_range_GeV[counter][1]);
+}
 
 //-----------
 // SmearEvent
@@ -30,11 +70,12 @@ void TAGHSmearer::SmearEvent(hddm_s::HDDM *record)
    hddm_s::HodoChannelList taghs = record->getHodoChannels();
    hddm_s::HodoChannelList::iterator iter;
    for (iter = taghs.begin(); iter != taghs.end(); ++iter) {
+      int counter = iter->getCounterId();
+      iter->setE(get_tagh_energy(counter));
       iter->deleteTaggerHits();
       hddm_s::TaggerTruthHitList thits = iter->getTaggerTruthHits();
       hddm_s::TaggerTruthHitList::iterator titer;
       for (titer = thits.begin(); titer != thits.end(); ++titer) {
-         int counter = *(int*)titer->getAttribute("counterId");
          if (tagh_config->counter_quality[counter] != 1)
             continue;
          // smear the time
--- src/programs/Simulation/mcsmear/TAGMSmearer.cc
+++ src/programs/Simulation/mcsmear/TAGMSmearer.cc
@@ -1,5 +1,7 @@
 #include "TAGMSmearer.h"
 
+tagm_config_t *tagm_config_instance(0);
+
 //-----------
 // tagm_config_t  (constructor)
 //-----------
@@ -17,6 +19,46 @@ tagm_config_t::tagm_config_t(JEventLoop *loop) {
        int rowcolumn = quality[i]["row"]*1000 + quality[i]["column"];
        fiber_quality[rowcolumn] = quality[i]["code"];
     }
+
+   std::map<string, float> beam_parms;
+   loop->GetCalib("PHOTON_BEAM/endpoint_energy", beam_parms);
+   endpoint_energy_GeV = beam_parms.at("PHOTON_BEAM_ENDPOINT_ENERGY");
+   std::map<string, float> beam_calib;
+   loop->GetCalib("PHOTON_BEAM/hodoscope/endpoint_calib", beam_calib);
+   endpoint_calib_GeV = endpoint_energy_GeV;
+   if (beam_calib.find("TAGGER_CALIB_ENERGY") != beam_calib.end()) {
+      endpoint_calib_GeV = beam_calib.at("TAGGER_CALIB_ENERGY");
+   }
+   std::vector<std::map<string, float> > micro_parms;
+   loop->GetCalib("PHOTON_BEAM/microscope/scaled_energy_range", micro_parms);
+   for (unsigned int i=0; i < micro_parms.size(); ++i) {
+      int col = micro_parms[i]["column"];
+      double Emin = micro_parms[i]["xlow"] * endpoint_calib_GeV
+                    + endpoint_energy_GeV - endpoint_calib_GeV;
+      double Emax = micro_parms[i]["xhigh"] * endpoint_calib_GeV
+                    + endpoint_energy_GeV - endpoint_calib_GeV;
+      energy_range_GeV[col] = {Emin, Emax};
+   }
+   tagm_config_instance = this;
+}
+
+double TAGMSmearer::get_tagm_energy(int column, int mid_low_high_rand)
+{
+   // mid_low_high_rand: =0 returns the center energy of the bin,
+   //                    =1 returns the low energy of the bin,
+   //                    =2 returns the high energy of the bin,
+   //                    =other returns a random value within the bin.
+   if (tagm_config_instance == 0)
+      return -1;
+   else if (mid_low_high_rand == 0)
+      return (tagm_config_instance->energy_range_GeV[column][0] +
+              tagm_config_instance->energy_range_GeV[column][1]) / 2;
+   else if (mid_low_high_rand == 1)
+      return tagm_config_instance->energy_range_GeV[column][0];
+   else if (mid_low_high_rand == 2)
+      return tagm_config_instance->energy_range_GeV[column][1];
+   return gDRandom.Uniform(tagm_config_instance->energy_range_GeV[column][0],
+                           tagm_config_instance->energy_range_GeV[column][1]);
 }
 
 //-----------
@@ -27,12 +69,13 @@ void TAGMSmearer::SmearEvent(hddm_s::HDDM *record)
    hddm_s::MicroChannelList tagms = record->getMicroChannels();
    hddm_s::MicroChannelList::iterator iter;
    for (iter = tagms.begin(); iter != tagms.end(); ++iter) {
+      int column = iter->getColumn();
+      int row = iter->getRow();
+      iter->setE(get_tagm_energy(column));
       iter->deleteTaggerHits();
       hddm_s::TaggerTruthHitList thits = iter->getTaggerTruthHits();
       hddm_s::TaggerTruthHitList::iterator titer;
       for (titer = thits.begin(); titer != thits.end(); ++titer) {
-         int column = *(int*)titer->getAttribute("column");
-         int row = *(int*)titer->getAttribute("row");
          if (tagm_config->fiber_quality[column + row*1000] != 1)
             continue;
          // smear the time
