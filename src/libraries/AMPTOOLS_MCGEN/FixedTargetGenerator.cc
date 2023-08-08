983f877a3b6cadf6312b845142d70efc0660567e
Matthew Shepherd <mashephe@indiana.edu>
2023-08-08 10:05:21 -0400

    extensive revisions to new fixed-target generator class

--- src/libraries/AMPTOOLS_MCGEN/FixedTargetGenerator.cc
+++ src/libraries/AMPTOOLS_MCGEN/FixedTargetGenerator.cc
@@ -8,28 +8,38 @@
 
 using namespace std;
 
-FixedTargetGenerator::FixedTargetGenerator( double beamEnergy, double targetMass,
+FixedTargetGenerator::FixedTargetGenerator( double photonBeamEnergy, double targetMass,
                                             const vector< double >& uvMasses,
                                             const vector< double >& lvMasses ) :
 m_uvMasses( uvMasses ),
 m_lvMasses( lvMasses ),
+m_lvMinUser( 0 ),
+m_lvMaxUser( 1E9 ),
+m_uvMinUser( 0 ),
+m_uvMaxUser( 1E9 ),
+m_tMagMinUser( 0 ),
+m_tMagMaxUser( 1E9 ),
+// by default we reweight any seed distributions put in by the user
+// to recover phase space
+m_reweightMask( kUpperVtxMass | kLowerVtxMass | kMomentumTransfer ),
 m_limitsValid( false )
 {
-  setBeamEnergy( beamEnergy );
+
+  setBeamEnergy( photonBeamEnergy );
   setTargetMass( targetMass );
   
   calculateLimits();
 }
 
 void
-FixedTargetGenerator::addUpperVertexBW( double mass, double width, double fraction = 1 ){
+FixedTargetGenerator::addUpperVtxBW( double mass, double width, double fraction = 1 ){
   
   m_upperBWGen.push_back( BreitWignerGenerator( mass, width ) );
   m_upperDecayChannel.addChannel( m_upperBWGen.size()-1, fraction );
 }
 
 void
-FixedTargetGenerator::addLowerVertexBW( double mass, double width, double fraction = 1 ){
+FixedTargetGenerator::addLowerVtxBW( double mass, double width, double fraction = 1 ){
   
   m_lowerBWGen.push_back( BreitWignerGenerator( mass, width ) );
   m_lowerDecayChannel.addChannel( m_lowerBWGen.size()-1, fraction );
@@ -40,16 +50,85 @@ FixedTargetGenerator::addMomentumTransfer( double tSlope, double fraction = 1 ){
   
   // we will always generate a negative argument:  exp( - t * tSlope  )
   // so take absolute value to avoid sign ambiguated
-  m_tSlopeGen.push_back( fabs( tSlope ) );
-  m_tSlopeChannel.addChannel( m_tSlopeGen.size(), fraction );
+
+  m_tSlopeVec.push_back( fabs( tSlope ) );
+  m_tSlopeChannel.addChannel( m_tSlopeVec.size()-1, fraction );
 }
+
+void
+FixedTargetGenerator::setBeamEnergy( double energy ){
+  
+  m_beam = TLorentzVector( 0, 0, energy, energy );
+  m_limitsValid = false;
+}
+
+void
+FixedTargetGenerator::setBeamP4( const TLorentzVector& p4 ){
+  
+  // if the beam is not aligned with z, then the logic in the
+  // algorithm for generating cos(theta) for user-specified
+  // momentum transfer will not work... it isn't general enough
+  // to handle those cases
+  if( p4.Perp() > 0 ){
+    
+    cerr << "FixedTargetGenerator: only beams in the z direction are supported" << endl;
+    exit( 1 );
+  }
+  
+  m_beam = p4;
+}
+
+void
+FixedTargetGenerator::setTargetMass( double mass ){
+  
+  m_target = TLorentzVector( 0, 0, 0, mass );
+  m_limitsValid = false;
+}
+
+void
+FixedTargetGenerator::setUpperVtxRange( double min, double max ){
+  
+  m_uvMinUser = min;
+  m_uvMaxUser = max;
+  m_limitsValid = false;
+}
+
+void
+FixedTargetGenerator::setLowerVtxRange( double min, double max ){
+  
+  m_lvMinUser = min;
+  m_lvMaxUser = max;
+  m_limitsValid = false;
+}
+
+void
+FixedTargetGenerator::setMomentumTransferRange( double min, double max ){
+  
+  // try to catch confusion with t and |t|...
+  //   a more informative message might be nice
+  assert( fabs( max ) > fabs( min ) );
+  
+  m_tMagMinUser = fabs( min );
+  m_tMagMaxUser = fabs( max );
+}
+
+void
+FixedTargetGenerator::setReweightMask( unsigned int mask ){
+  
+  // we are only using the 3 lowest bits of the int
+  assert( mask < 8 );
+  m_reweightMask = mask;
+}
+
+// **
+// const member functions:
 
 Kinematics*
 FixedTargetGenerator::generate() const {
 
   if( !m_limitsValid ) calculateLimits();
 
-  double lvM, uvM;
+  double lvM, uvM, cosThetaCM;
   vector< double > lvPairs;
   vector< double > uvPairs;
 
@@ -61,19 +140,27 @@ FixedTargetGenerator::generate() const {
       
       // first need to get two kinematically allowed
       // values for the upper and lower vertex masses
+      // and the scattering angle in the CM frame
+      
+      double t = 0;
       do {
         
         seedWeight = 1;
         
         pair< double, double > tmp = genLowerVertexMass();
         lvM = tmp.first;
-        seedWeight *= tmp.second;
+        if( m_reweightMask & kLowerVtxMass ) seedWeight *= tmp.second;
         
         tmp = genUpperVertexMass();
         uvM = tmp.first;
-        seedWeight *= tmp.second;
+        if( m_reweightMask & kUpperVtxMass ) seedWeight *= tmp.second;
+      
+        tmp = genCosThetaCM( uvM, lvM, &t );
+        cosThetaCM = tmp.first;
+        if( m_reweightMask & kMomentumTransfer ) seedWeight *= tmp.second;
       }
-      while( lvM + uvM > m_W );
+      while( ( lvM + uvM > m_W ) ||
+             ( fabs( cosThetaCM ) > 1  ) );
       
       lvPairs = genPairMasses( m_lvMax, m_lvMasses );
       uvPairs = genPairMasses( m_uvMax, m_uvMasses );
@@ -92,6 +179,16 @@ FixedTargetGenerator::generate() const {
     lvPairs.push_back( lvM );
     uvPairs.push_back( uvM );
     
+    // here we are computing the weights to recover N-body
+    // phase space from events that are drawn uniform in
+    // mass at the upper and lower vertex -- this means the
+    // while( ) statement at the bottom will do accept/reject
+    // on the phase space weight only.  This produces events
+    // of unity weight distributed by phase space.  These events
+    // may be further modified by user "seeds" and weighted
+    // by seedWeight to recover phase space.  True phase space
+    // will only be recovered if m_reweightMask = 7 (all bits on).
+    
     psWeight = pcm( m_W, lvM, uvM )/(4*PI*m_W);
     
     psWeight *= vertexPS( uvPairs, m_uvMasses );
@@ -105,8 +202,8 @@ FixedTargetGenerator::generate() const {
       
   // now generate the total upper and lower vertex four-vectors
   pair< TLorentzVector, TLorentzVector >
-    top = decay( ( m_beam + m_target ), uvM, lvM );
-  
+    top = decay( ( m_beam + m_target ), uvM, lvM, cosThetaCM );
+    
   // from these generate the cascade of four-vectors from each vertex
   vector< TLorentzVector > uvP4 = vertexGenP4( top.first, uvPairs, m_uvMasses );
   vector< TLorentzVector > lvP4 = vertexGenP4( top.second, lvPairs, m_lvMasses );
@@ -118,19 +215,6 @@ FixedTargetGenerator::generate() const {
   return new Kinematics( allP4, seedWeight );
 }
 
-void
-FixedTargetGenerator::setBeamEnergy( double energy ){
-  
-  m_beam = TLorentzVector( 0, 0, energy, energy );
-  m_limitsValid = false;
-}
-
-void
-FixedTargetGenerator::setTargetMass( double mass ){
-  
-  m_target = TLorentzVector( 0, 0, 0, mass );
-  m_limitsValid = false;
-}
 
 
 // the second value in the pair is the weight needed
@@ -149,9 +233,8 @@ FixedTargetGenerator::genUpperVertexMass() const {
 
   pair< double, double > massWeight;
   do{
-
-    massWeight =
-      m_upperBWGen[m_upperDecayChannel()]();
+    
+    massWeight = m_upperBWGen[m_upperDecayChannel()]();
     
     // the BW generator returns values that are flat
     // in 2-body phase space (s) reweight to make it
@@ -182,8 +265,7 @@ FixedTargetGenerator::genLowerVertexMass() const {
   pair< double, double > massWeight;
   do{
 
-    massWeight =
-      m_lowerBWGen[m_lowerDecayChannel()]();
+    massWeight = m_lowerBWGen[m_lowerDecayChannel()]();
     
     // the BW generator returns values that are flat
     // in 2-body phase space (s) reweight to make it
@@ -197,6 +279,56 @@ FixedTargetGenerator::genLowerVertexMass() const {
   return massWeight;
 }
 
+pair< double, double >
+FixedTargetGenerator::genCosThetaCM( double uvMass, double lvMass, double* t ) const {
+
+  pair< double, double > cosThetaWeight;
+
+  double eBeamCM = ( m_W*m_W + m_beam.M2() - m_target.M2() ) / ( 2*m_W );
+  double puvCM = pcm( m_W, uvMass, lvMass );
+  double euvCM = sqrt( puvCM*puvCM + uvMass*uvMass );
+  
+  do{
+    
+    if( m_tSlopeVec.size() == 0 ) {
+      
+      // this is pure phase space:
+      cosThetaWeight.first = m_randGen.Uniform( -1, 1 );
+      cosThetaWeight.second = 1;
+      
+      // no seeding with exponentials -- compute t from cos( theta )
+      *t = cosThetaWeight.first * ( 2 * pcm( m_W, m_beam.M(), m_target.M() ) * puvCM ) -
+      ( 2*eBeamCM*euvCM - m_beam.M2() - uvMass*uvMass );
+    }
+    else{
+      
+      // if we get here, we're going to generate t and then use it compute
+      // cos( theta ) prior to returning
+      
+      // get a tSlope according to the mix of slopes specified by the user
+      double tSlope = m_tSlopeVec[m_tSlopeChannel()];
+      
+      // the ROOT generator generates exponential decay so tSlope should be positive
+      *t = -m_randGen.Exp( 1./tSlope );
+      
+      // ref: Martin and Spearman Eq. 4.81b
+      cosThetaWeight.first = ( *t + 2*eBeamCM*euvCM - m_beam.M2() - uvMass*uvMass ) /
+      ( 2 * pcm( m_W, m_beam.M(), m_target.M() ) * puvCM );
+      
+      // dN / d(cos theta ) = 2 q q' [ dN / dt ] ... to make the LHS
+      // uniform (phase space) we weight by the inverse of the RHS
+      // (t is negative in this expression and tSlope is positive)
+      cosThetaWeight.second = 1 /
+         ( 2 * pcm( m_W, m_beam.M(), m_target.M() ) * puvCM * exp( *t * tSlope ) );
+    }
+  }
+  while( ( fabs( *t ) < m_tMagMinUser ) ||
+         ( fabs( *t ) > m_tMagMaxUser ) );
+    
+  return cosThetaWeight;
+}
+
+
 void
 FixedTargetGenerator::calculateLimits() const {
   
@@ -209,11 +341,14 @@ FixedTargetGenerator::calculateLimits() const {
   double uvSum = 0;
   for( unsigned int i = 0; i < m_uvMasses.size(); ++i ) uvSum += m_uvMasses[i];
   
-  m_lvMin = lvSum;
-  m_lvMax = m_W - uvSum;
+  // set the generation limits considering also any user-specified
+  // range of masses that may be more restrictive than the kinematic limits
   
-  m_uvMin = uvSum;
-  m_uvMax = m_W - lvSum;
+  m_lvMin = ( m_lvMinUser > lvSum ? m_lvMinUser : lvSum );
+  m_lvMax = ( m_lvMaxUser < m_W - uvSum ? m_lvMaxUser : m_W - uvSum );
+  
+  m_uvMin = ( m_uvMinUser > uvSum ? m_uvMinUser : uvSum );;
+  m_uvMax = ( m_uvMaxUser < m_W - lvSum ? m_uvMaxUser : m_W - lvSum );
   
   // now calculate the max weight by computing the products of the maximum
   // relative momentum at every step of the decay
@@ -249,7 +384,8 @@ FixedTargetGenerator::calculateLimits() const {
 }
 
 vector< double >
-FixedTargetGenerator::genPairMasses( double max, const vector< double >& masses ) const {
+FixedTargetGenerator::genPairMasses( double max,
+                                     const vector< double >& masses ) const {
   
   if( masses.size() == 1 ) return masses;
   
@@ -257,7 +393,6 @@ FixedTargetGenerator::genPairMasses( double max, const vector< double >& masses 
   // masses of the intermediate states -- one should have the decays:
   //    pairMasses[i] -> pairMasses[i-1] + masses[i]
   // for convenience we set pairMasses[0] = masses[0]
-  //
   
   double min = 0;
   for( unsigned int i = 0; i < masses.size(); ++i ) min += masses[i];
@@ -349,19 +484,37 @@ FixedTargetGenerator::pcm( double ecm, double m1, double m2 ) const {
   double lambda = ( ecm*ecm - m1*m1 - m2*m2 )*
                   ( ecm*ecm - m1*m1 - m2*m2 ) -
                   4*m1*m1*m2*m2;
+  
   return sqrt( lambda ) / ( 2 * ecm );
+}
+
+
+pair< TLorentzVector, TLorentzVector >
+FixedTargetGenerator::decay( const TLorentzVector& p4Initial,
+                            double m1, double m2 ) const {
+  
+  return decay( p4Initial, m1, m2, m_randGen.Uniform( -1, 1 ) );
 }
 
 pair< TLorentzVector, TLorentzVector >
 FixedTargetGenerator::decay( const TLorentzVector& p4Initial,
-                             double m1, double m2 ) const {
+                             double m1, double m2, double cosTheta ) const {
+  
+  // this method allows specification of cosTheta in the convention
+  // where theta is angle w.r.t. z-axis in the rest frame of
+  // p4Initial... doing so is probably only generally useful when
+  // p4Initial is along z (in this case theta is the more-standard
+  // helicity angle) -- use caution if you decide to copy this
+  // function for genreal use elsewhere!!
   
   pair< TLorentzVector, TLorentzVector > p4Final;
   
   // first generate the decay of back to back particles
   // in the center of momentum frame
   p4Final.first.SetXYZM( 0, 0, pcm( p4Initial.M(), m1, m2 ), m1 );
-  p4Final.first.RotateY( acos( m_randGen.Uniform( -1, 1 ) ) );
+
+  // then rotate appropriately
+  p4Final.first.RotateY( acos( cosTheta ) );
   p4Final.first.RotateZ( m_randGen.Uniform( 0, 2*PI ) );
   p4Final.second.SetVectM( -p4Final.first.Vect(), m2 );
   
--- src/libraries/AMPTOOLS_MCGEN/FixedTargetGenerator.h
+++ src/libraries/AMPTOOLS_MCGEN/FixedTargetGenerator.h
@@ -5,8 +5,8 @@
 #include "TLorentzVector.h"
 #include "TH1.h"
 
-#include "DecayChannelGenerator.h"
-#include "BreitWignerGenerator.h"
+#include "AMPTOOLS_MCGEN/DecayChannelGenerator.h"
+#include "AMPTOOLS_MCGEN/BreitWignerGenerator.h"
 
 #include <vector>
 
@@ -18,33 +18,76 @@ class FixedTargetGenerator {
   
 public:
   
+  enum { kMomentumTransfer = 1, kUpperVtxMass = 2, kLowerVtxMass = 4 };
+  
   FixedTargetGenerator(){}
   
-  FixedTargetGenerator( double beamEnergy, double targetMass,
+  // this constructor assumes the beam is a photon (go GlueX!)  --
+  // more general cases can be handled by setBeamP4 below
+  FixedTargetGenerator( double photonBeamEnergy, double targetMass,
                         const vector< double >& uvMasses,
                         const vector< double >& lvMasses );
-    
+
   Kinematics* generate() const;
 
-  void setUpperVtxMasses( const vector< double >& uvMasses );
-  void addUpperVertexBW( double mass, double width, double fraction );
-
-  void setLowerVtxMasses( const vector< double >& lvMasses );
-  void addLowerVertexBW( double mass, double width, double fraction );
-
-  void setTargetMass( double targetMass );
+  // These functions setup the inital state.
   void setBeamEnergy( double eBeam );
+  void setBeamP4( const TLorentzVector& p4 );
+  void setTargetMass( double targetMass );
+
+  // Use to set the masses of the stable particles produced
+  // at the upper and lower verticies
+  void setUpperVtxMasses( const vector< double >& uvMasses );
+  void setLowerVtxMasses( const vector< double >& lvMasses );
+
+  // This is used to seed generation with a BW to allow the user
+  // to concentrante events in regions of mass where the amplitude
+  // might peak.  If the kUpperVtxMass and/or kLowerVtxMass bits
+  // are turned on in the reweight mask then weights will be provided
+  // to smooth out these peaks in one or both distributions.
+  void addUpperVtxBW( double mass, double width, double fraction );
+  void addLowerVtxBW( double mass, double width, double fraction );
+
+  // specify a range of mass to generate -- otherwise the full
+  // kinematic limits are used
+  void setUpperVtxRange( double min, double max );
+  void setLowerVtxRange( double min, double max );
   
-  void setBeamP4( const TLorentzVector& p4 ){ m_beam = p4; }
-  
+  // these should be absolute values:  |t|
+  void setMomentumTransferRange( double min, double max );
   void addMomentumTransfer( double tSlope, double fraction );
+
+  // The mask a bitmask formed with the enums above -- the
+  // default behavior is achieved by:
+  //
+  // setReweightMask( kMomentumTransfer | kUpperVtxMass | kLowerVtxMass );
+  //
+  // In this case if the user inputs either BW distributions for
+  // the vertices or exponential t slopes then then the events will
+  // be generated with a weight that recovers pure phase space.
+  //
+  // A desirale option for some applications may be the following:
+  //
+  // addMomentumTransfer( 2.0 );
+  // setReweightMask( kUpperVtxMass | kLowerVtxMass );
+  //
+  // In this case events will be generated with e^-2*|t| and, additional
+  // upper vertex and lower vertex BW's as desired.  The weight with the
+  // events will unweight the BW's but no the t-distribution.  The
+  // corresponding mass distributions then will no longer be pure phase
+  // space but what one gets when phase space is weighted by e^-2*|t|
   
+  void setReweightMask( unsigned int mask );
+
 private:
 
   void calculateLimits() const;
 
+  double pcm( double ecm, double m1, double m2 ) const;
+
   pair< double, double > genUpperVertexMass() const;
   pair< double, double > genLowerVertexMass() const;
+  pair< double, double > genCosThetaCM( double uvM, double lvM, double* t ) const;
   
   vector< double > genPairMasses( double max, const vector< double >& masses ) const;
 
@@ -57,9 +100,9 @@ private:
 
   pair< TLorentzVector, TLorentzVector >
        decay( const TLorentzVector& p4Initial, double m1, double m2 ) const;
+  pair< TLorentzVector, TLorentzVector >
+       decay( const TLorentzVector& p4Initial, double m1, double m2, double cosThetaCM ) const;
   
-  double pcm( double ecm, double m1, double m2 ) const;
-
   mutable TRandom m_randGen;
   
   TLorentzVector m_beam;
@@ -68,6 +111,14 @@ private:
   vector< double > m_uvMasses;
   vector< double > m_lvMasses;
   vector< double > m_tSlope;
+  
+  double m_lvMinUser;
+  double m_lvMaxUser;
+  double m_uvMinUser;
+  double m_uvMaxUser;
+  double m_tMagMinUser;
+  double m_tMagMaxUser;
+  unsigned int m_reweightMask;
   
   mutable double m_W;
   mutable double m_uvMax;
@@ -84,7 +135,7 @@ private:
   
   vector< BreitWignerGenerator > m_upperBWGen;
   vector< BreitWignerGenerator > m_lowerBWGen;
-  vector< double > m_tSlopeGen;
+  vector< double > m_tSlopeVec;
 };
 
 #endif
