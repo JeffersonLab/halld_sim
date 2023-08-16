680bc17975a32313b96d3f5b51d81b8a4b8773d0
Amy Schertz <amschertz@email.wm.edu>
2023-08-16 14:05:48 -0400

    add 2D angular histograms to OmegaPiPlotGenerator

--- src/libraries/AMPTOOLS_DATAIO/OmegaPiPlotGenerator.h
+++ src/libraries/AMPTOOLS_DATAIO/OmegaPiPlotGenerator.h
@@ -17,7 +17,7 @@ class OmegaPiPlotGenerator : public PlotGenerator
 public:
   
   // create an index for different histograms
-  enum { kOmegaPiMass = 0, kCosTheta = 1, kPhi = 2, kCosThetaH = 3, kPhiH = 4, kProd_Ang = 5, kt = 6, kRecoilMass = 7, kTwoPiMass = 8, kProtonPiMass = 9, kRecoilPiMass = 10, kLambda = 11, kDalitz = 12, kPhiDelta = 13, kCosThetaDelta = 14, kNumHists};
+  enum { kOmegaPiMass = 0, kCosTheta = 1, kPhi = 2, kCosThetaH = 3, kPhiH = 4, kProd_Ang = 5, kt = 6, kRecoilMass = 7, kTwoPiMass = 8, kProtonPiMass = 9, kRecoilPiMass = 10, kLambda = 11, kDalitz = 12, kPhiDelta = 13, kCosThetaDelta = 14, kOmegaPiAngles = 15, kOmegaHAngles = 16, kDeltaAngles = 17, kBigLittlePhi = 18, kNumHists};
 
   OmegaPiPlotGenerator( const FitResults& results, Option opt);
   OmegaPiPlotGenerator( const FitResults& results );
