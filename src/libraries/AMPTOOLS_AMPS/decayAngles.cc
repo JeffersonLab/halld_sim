a562fa047549a12848ec1695eef67c328ffae9a5
Justin Stevens <jrsteven@jlab.org>
2023-09-12 14:13:15 -0400

    change lab to CM for calculation of prodPhi

--- src/libraries/AMPTOOLS_AMPS/decayAngles.cc
+++ src/libraries/AMPTOOLS_AMPS/decayAngles.cc
@@ -83,7 +83,7 @@ double getPhiProd(double polAngle, TLorentzVector parentLab, TLorentzVector beam
 
 	TVector3 eps( cos( polAngle ), sin( polAngle ), 0.0 );
 
-	double phiProd = atan2( y.Dot( eps ), inverseLab.Vect().Unit().Dot( eps.Cross( y ) ) );
+	double phiProd = atan2( y.Dot( eps ), inverseCM.Vect().Unit().Dot( eps.Cross( y ) ) );
 
 	return phiProd;	
 } 
