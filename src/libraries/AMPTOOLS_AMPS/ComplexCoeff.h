0b329c52dc9ca83fcfe7d1ff78d26066d10316d8
Matthew Shepherd <mashephe@h1.quartz.uits.iu.edu>
2024-04-11 17:44:48 -0400

    use member data to store complex double value and update it with updatePar to minimize expensive polar calls

--- src/libraries/AMPTOOLS_AMPS/ComplexCoeff.h
+++ src/libraries/AMPTOOLS_AMPS/ComplexCoeff.h
@@ -16,11 +16,6 @@ using namespace std;
 
 class Kinematics;
 
-#ifdef GPU_ACCELERATION
-void GPUComplexCoeff_exec(dim3 dimGrid, dim3 dimBlock, GPU_AMP_PROTO, GDouble param1, GDouble param2, bool represReIm);
-#endif
-
-
 class ComplexCoeff : public UserAmplitude< ComplexCoeff >
 {  
 public:
@@ -33,20 +28,16 @@ public:
   string name() const { return "ComplexCoeff"; }
   
   complex< GDouble > calcAmplitude( GDouble** pKin ) const;
-      
-#ifdef GPU_ACCELERATION
-  void launchGPUKernel( dim3 dimGrid, dim3 dimBlock, GPU_AMP_PROTO) const{
-	  GPUComplexCoeff_exec(dimGrid, dimBlock, GPU_AMP_ARGS, m_param1, m_param2, m_represReIm);
-  };
-  
-  bool isGPUEnabled() const { return true; }
-#endif
+
+  void updatePar( const AmpParameter& par );
 
 private:
 	
   AmpParameter m_param1;
   AmpParameter m_param2;
   bool m_represReIm;
+
+  complex< GDouble > m_value;
 };
 
 #endif
