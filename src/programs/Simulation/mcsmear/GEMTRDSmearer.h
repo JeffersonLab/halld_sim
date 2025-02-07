5b2ed8679657d5e98fd1cd280914bcc3646b0d0d
staylorjlab <staylor@ifarm2402.jlab.org>
2025-02-07 16:24:04 -0500

    add version of GEMTRD smearer that distributes charge over multiple strips.

---
+++ src/programs/Simulation/mcsmear/GEMTRDSmearer.h
@@ -0,0 +1,53 @@
+// Smearing class for GEMTRD
+
+#ifndef _GEMTRDSMEARER_H_
+#define _GEMTRDSMEARER_H_
+
+#include "Smearer.h"
+
+class gemtrd_config_t 
+{
+  public:
+  gemtrd_config_t(JEventLoop *loop);
+  
+  // GEMTRD resolutions and threshold
+  double GEMTRD_TSIGMA;
+  double GEMTRD_ASIGMA;
+  double GEMTRD_THRESHOLD;
+  double GEMTRD_XYSIGMA;
+
+  double GEMTRD_INTEGRAL_TO_AMPLITUDE;
+};
+
+
+class GEMTRDSmearer : public Smearer
+{
+  public:
+  GEMTRDSmearer(JEventLoop *loop, mcsmear_config_t *in_config) : Smearer(loop, in_config) {
+    gemtrd_config = new gemtrd_config_t(loop);
+  }
+  ~GEMTRDSmearer() {
+    delete gemtrd_config;
+  }
+  
+  void SmearEvent(hddm_s::HDDM *record);
+  double GetStripCharge(double q,int strip) const;
+  
+private:
+  class strip_hit_t{
+  public:
+    strip_hit_t(int plane,int strip,double q,double t):plane(plane),strip(strip),q(q),t(t){}
+    int plane;
+    int strip;
+    double q;
+    double t;
+  };
+
+  gemtrd_config_t  *gemtrd_config;
+};
+
+
+
+
+#endif // _GEMTRDSMEARER_H_
+
