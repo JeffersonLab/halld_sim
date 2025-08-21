a32da6666095af1b88b916db6b7c1b9e839a23bb
kevScheuer <kscheuer@jlab.org>
2025-08-21 13:44:40 -0400

    Relaxed the requirements for successful fit
    This allows an eMatrixStatus of 2, corresponding
    to a forced positive definite matrix, to be
    considered a successful fit.

--- src/programs/AmplitudeAnalysis/fitMPI/fitMPI.cc
+++ src/programs/AmplitudeAnalysis/fitMPI/fitMPI.cc
@@ -86,7 +86,7 @@ double runSingleFit(ConfigurationInfo* cfgInfo, bool useMinos, bool hesse, int m
       if(hesse)
          fitManager->hesseEvaluation();
 
-      fitFailed = ( fitManager->status() != 0 || fitManager->eMatrixStatus() != 3 );
+      fitFailed = ( fitManager->status() != 0 || fitManager->eMatrixStatus() == 0 || fitManager->eMatrixStatus() == 1 );
 
       if( fitFailed )
          cout << "ERROR: fit failed use results with caution..." << endl;
