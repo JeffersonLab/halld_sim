a2a1010fbf46831d0e5fcee45e900462c7b3701a
Justin Stevens <jrsteven@jlab.org>
2025-09-29 21:31:05 -0400

    add data reader with dummy event to return number of generated events

---
+++ src/libraries/AMPTOOLS_DATAIO/ROOTDataReaderNumGen.cc
@@ -0,0 +1,122 @@
+
+#include <vector>
+#include <cassert>
+#include <iostream>
+
+#include "TLorentzVector.h"
+
+#include "AMPTOOLS_DATAIO/ROOTDataReaderNumGen.h"
+#include "IUAmpTools/Kinematics.h"
+
+#include "TH1.h"
+#include "TFile.h"
+#include "TTree.h"
+
+using namespace std;
+
+ROOTDataReaderNumGen::ROOTDataReaderNumGen( const vector< string >& args ):
+  UserDataReader< ROOTDataReaderNumGen >( args ),
+  m_eventCounter( 0 ),
+  m_useWeight( false )
+{
+  assert( args.size() == 2 || args.size() == 1 );
+  
+  TH1::AddDirectory( kFALSE );
+  
+  //this way of opening files works with URLs of the form
+  // root://xrootdserver/path/to/myfile.root
+  m_inFile = TFile::Open( args[0].c_str() );
+
+  
+  // default to tree name of "kin" if none is provided
+  if( args.size() == 1 ){
+    
+    m_inTree = dynamic_cast<TTree*>( m_inFile->Get( "kin" ) );
+  }
+  else{
+    
+    m_inTree = dynamic_cast<TTree*>( m_inFile->Get( args[1].c_str() ) );
+  }
+  
+  m_inTree->SetBranchAddress( "NumFinalState", &m_nPart );
+  m_inTree->SetBranchAddress( "E_FinalState", m_e );
+  m_inTree->SetBranchAddress( "Px_FinalState", m_px );
+  m_inTree->SetBranchAddress( "Py_FinalState", m_py );
+  m_inTree->SetBranchAddress( "Pz_FinalState", m_pz );
+  m_inTree->SetBranchAddress( "E_Beam", &m_eBeam );
+  m_inTree->SetBranchAddress( "Px_Beam", &m_pxBeam );
+  m_inTree->SetBranchAddress( "Py_Beam", &m_pyBeam );
+  m_inTree->SetBranchAddress( "Pz_Beam", &m_pzBeam );
+
+  if(m_inTree->GetBranch("Weight") != NULL) {
+
+    m_useWeight = true;
+    m_inTree->SetBranchAddress( "Weight", &m_weight );
+  }
+  else{
+
+    m_useWeight = false;
+  }
+}
+
+ROOTDataReaderNumGen::~ROOTDataReaderNumGen()
+{
+  if( m_inFile != NULL ) m_inFile->Close();
+}
+
+void
+ROOTDataReaderNumGen::resetSource()
+{
+	
+  cout << "Resetting source " << m_inTree->GetName() 
+       << " in " << m_inFile->GetName() << endl;
+  
+  // this will cause the read to start back at event 0
+  m_eventCounter = 0;
+}
+
+Kinematics*
+ROOTDataReaderNumGen::getEvent()
+{
+  if( m_eventCounter < 1 ){
+    
+    m_inTree->GetEntry( m_eventCounter++ );
+    assert( m_nPart < Kinematics::kMaxParticles );
+    
+    vector< TLorentzVector > particleList;
+    
+    particleList.
+      push_back( TLorentzVector( m_pxBeam, m_pyBeam, m_pzBeam, m_eBeam ) );
+    
+    for( int i = 0; i < m_nPart; ++i ){
+      
+      particleList.push_back( TLorentzVector( m_px[i], m_py[i], m_pz[i], m_e[i] ) );
+    }
+ 
+    // get sum of weights
+    double sumW = 0;
+    if(m_useWeight) {   
+      int n = m_inTree->GetEntries();
+
+      for (int j = 0; j < n; j++) {
+        m_inTree->GetEntry(j);
+    	sumW += m_weight;
+      }
+    }
+    else {
+      sumW = m_inTree->GetEntries();
+    }
+
+    return new Kinematics( particleList, sumW );
+  }
+  else{
+    
+    return NULL;
+  }
+}
+
+unsigned int
+ROOTDataReaderNumGen::numEvents() const
+{	
+  return static_cast< unsigned int >( m_inTree->GetEntries() );
+}
